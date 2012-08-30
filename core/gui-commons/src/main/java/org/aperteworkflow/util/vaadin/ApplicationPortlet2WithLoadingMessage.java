b2f56ed10de61e59d4db276aeb372fc51187fdad
Piotrek <polszewski@bluesoft.net.pl>
2012-08-30 19:12:59 +0200

    dict caching + loader message fixes
    1. dictionary dao is using expiring cache instead of round robin one
    2. loader message now hides correctly when using multiple portlet
    instances on the same page
    3. loader message is now localized

--- core/gui-commons/src/main/java/org/aperteworkflow/util/vaadin/ApplicationPortlet2WithLoadingMessage.java
+++ core/gui-commons/src/main/java/org/aperteworkflow/util/vaadin/ApplicationPortlet2WithLoadingMessage.java
@@ -4,6 +4,8 @@ import com.vaadin.Application;
 import com.vaadin.terminal.gwt.server.ApplicationPortlet2;
 import com.vaadin.terminal.gwt.server.PortletApplicationContext2;
 import com.vaadin.ui.Window;
+import pl.net.bluesoft.rnd.util.i18n.I18NSource;
+import pl.net.bluesoft.rnd.util.i18n.I18NSourceFactory;
 
 import javax.portlet.PortletConfig;
 import javax.portlet.PortletException;
@@ -21,16 +23,20 @@ public class ApplicationPortlet2WithLoadingMessage extends ApplicationPortlet2 {
 	@Override
 	protected void writeAjaxPageHtmlVaadinScripts(RenderRequest request, RenderResponse response, BufferedWriter writer, Application application, String themeName) throws IOException, PortletException {
 		response.createResourceURL().setParameter("img", "loader");
-        //TODO - i18n
-		writer.write(String.format("<div id='%s'>Please wait, loading...</div>", getLoaderTagId(getPortletConfig())));
+
+		I18NSource i18NSource = I18NSourceFactory.createI18NSource(request.getLocale());
+		writer.write(String.format("<div name='%s'>%s</div>",
+				getLoaderTagId(getPortletConfig()),
+				i18NSource.getMessage("loader.message")));
+
 		super.writeAjaxPageHtmlVaadinScripts(request, response,	writer,	application, themeName);
 	}	
 
 	public static void hideLoadingMessage(Window window, PortletApplicationContext2 context) {
-		String js = "if (document.getElementById) {" +
-				"	var el = document.getElementById('%s');" +
-				"	if (el) {" +
-				"		el.style.display = 'none';" +
+		String js = "if (document.getElementsByName) {" +
+				"	var elems = document.getElementsByName('%s');" +
+				"	for (var i = 0; i < elems.length; ++i) {" +
+				"		elems[i].style.display = 'none';" +
 				"	}" +
 				"}";
 		window.executeJavaScript(String.format(js, getLoaderTagId(context.getPortletConfig())));
