7057ccb4c472905b4a51711fdc026c2c659459cf
Piotrek <polszewski@bluesoft.net.pl>
2012-10-05 16:47:43 +0200

    sonar

---
+++ utils/aperte-utils/src/main/java/pl/net/bluesoft/rnd/pt/utils/lang/Lang2.java
@@ -0,0 +1,35 @@
+package pl.net.bluesoft.rnd.pt.utils.lang;
+
+import java.util.Collection;
+
+/**
+ * User: POlszewski
+ * Date: 2012-10-05
+ * Time: 10:29
+ */
+public class Lang2 {
+	public static byte[] noCopy(byte[] t) {
+		return t;
+	}
+
+	public static <T> T[] noCopy(T[] t) {
+		return t;
+	}
+
+	public static String[] toStringArray(Collection<String> collection) {
+		if (collection != null) {
+			return collection.toArray(new String[collection.size()]);
+		}
+		return null;
+	}
+
+	public static <T> T assumeType(Object obj, Class<T> clazz) {
+		if (obj == null) {
+			return null;
+		}
+		if (clazz.isAssignableFrom(obj.getClass())) {
+			return (T)obj;
+		}
+		throw new ClassCastException("Unable to cast " + obj.getClass() + " to " + clazz);
+	}
+}
